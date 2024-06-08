2fe522c51eea915e1c73b387dec3622c80381829
MK <muchunumike@gmail.com>
2024-06-09 02:23:45 +0300

    f(register user): Expose connection to a global variable and automigrate user in models

--- database/connection.go
+++ database/connection.go
@@ -1,13 +1,21 @@
 package database
 
 import (
+	"github.com/mike-kimani/goauth/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
-	_, err := gorm.Open(mysql.Open("root:novek@/jwt_go_auth"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open("root:novek@/jwt_go_auth"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
+	}
+	DB = connection
+	err = connection.AutoMigrate(&models.User{})
+	if err != nil {
+
 	}
 }
