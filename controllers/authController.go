60bfbb8923286d4d40396b9779a37187d60aa7a9
MK <muchunumike@gmail.com>
2024-06-09 01:48:22 +0300

    f(register user): Get user infofrom user and return it as a JSON

--- controllers/authController.go
+++ controllers/authController.go
@@ -5,3 +5,14 @@ import "github.com/gofiber/fiber/v2"
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, World👋")
 }
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+	err := c.BodyParser(&data)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(data)
+}
