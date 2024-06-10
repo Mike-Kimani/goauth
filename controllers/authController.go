d09ca4a2446525c66234fe2d5179230b8261d50a
MK <muchunumike@gmail.com>
2024-06-10 14:42:02 +0300

    f(scale_certification): Implement user login and generate cookie with token

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,15 +1,20 @@
 package controllers
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"github.com/mike-kimani/goauth/database"
 	"github.com/mike-kimani/goauth/models"
 	"golang.org/x/crypto/bcrypt"
+	"strconv"
+	"time"
 )
 
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, World👋")
 }
+
+const SecretKey = "secret"
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -28,4 +33,52 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	err := c.BodyParser(&data)
+	if err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.Id == 0 {
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+
+	err = bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"]))
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "error",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
