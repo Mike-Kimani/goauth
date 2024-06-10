c0f672a0ac60f0609181914814445c7e3f2ddcbb
MK <muchunumike@gmail.com>
2024-06-10 14:53:06 +0300

    f(register_user): Implement user login

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/mike-kimani/goauth/database"
 	"github.com/mike-kimani/goauth/routes"
 	"log"
@@ -11,8 +12,14 @@ func main() {
 	database.Connect()
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:8000",
+		AllowCredentials: true,
+	}))
+
 	routes.Setup(app)
 	routes.Register(app)
+	routes.Login(app)
 
 	err := app.Listen(":8000")
 
